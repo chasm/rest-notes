60fd60a0c9bcd73ad0805932e5d43a366ce7d0d2
Charles F. Munat <charles.munat@gmail.com>
2015-07-30 10:48:11 +1200

    Add password methods to User.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
+require 'bcrypt'
+
 class User < ActiveRecord::Base
+  include BCrypt
+
+  def password
+    @password ||= Password.new(digest)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.digest = @password
+  end
 end
