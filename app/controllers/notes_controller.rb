18c2447d5c5928784bd9a0ccd508875452873ffe
Charles F. Munat <charles.munat@gmail.com>
2015-07-30 11:28:10 +1200

    Add notes controller.

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,63 @@
+require 'glorify'
+
+# GET routes return HTML
+
+# LIST
+get '/notes' do
+  @notes = Note.order :created_at
+  @title = "REST Notes"
+  erb :"/notes/index"
+end
+
+# NEW (form)
+get '/notes/new' do
+  @title = "Add note :: REST Notes"
+  erb :"/notes/new"
+end
+
+# SHOW
+get '/notes/:id' do
+  notes = Note.order :created_at
+  @note = Note.find params[:id]
+  @title = "Show note :: REST Notes"
+
+  idx = notes.find_index @note
+
+  @prev = idx == 0 ? nil : notes[idx - 1]
+  @next = idx == (notes.length - 1) ? nil : notes[idx + 1]
+
+  erb :"/notes/show"
+end
+
+# EDIT (form)
+get '/notes/:id/edit' do
+  @note = Note.find params[:id]
+  @title = "Edit note :: REST Notes"
+
+  erb :"/notes/edit"
+end
+
+# NON-GET route redirect to GET routes
+
+# CREATE
+post '/notes' do
+  note = Note.create params[:note]
+
+  redirect "/notes/#{note.id}"
+end
+
+# UPDATE
+patch '/notes/:id' do
+  note = Note.find params[:id]
+  note.update_attributes params[:note]
+
+  redirect "/notes/#{note.id}"
+end
+
+# DESTROY
+delete '/notes/:id' do
+  note = Note.find params[:id]
+  note.destroy
+
+  redirect '/notes'
+end
